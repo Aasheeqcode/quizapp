58cd96710f974e297bbf1f2e949e816af6ebb178
S.Mohameed Aasheeq <151372443+Aasheeqcode@users.noreply.github.com>
2024-08-11 18:16:12 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,13 @@
+document.getElementById("gklevel1").onclick=function(){
+    window.location.href='quizapp.html'
+}
+
+document.getElementById("gklevel2").onclick=function(){
+    
+}
+document.getElementById("gklevel3").onclick=function(){
+    
+}
+document.getElementById("gklevel4").onclick=function(){
+    
+}
